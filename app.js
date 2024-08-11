ac17e939ae922d6b0745b7599abb7c0381173ae3
ddook <dukeddoan3@gmail.com>
2024-08-10 19:02:31 -0700

    added a floating html demo

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const PORT = 3003;
-
-
-app.get('/floatdiv', (_req, res) => {
-    res.sendFile(path.join(__dirname, 'floatdiv.html'));
-});
-
-app.get('/test', (_req, res) => {
+app.get('/test', (req, res) => {
     res.sendFile(path.join(__dirname, 'test.html'));
 });
-
 // Define the route to serve the HTML file
-app.get('/', (_req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'home.html'));
 });
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
